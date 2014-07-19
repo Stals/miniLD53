acbb8927ecdd3386d0769ef18a1e56a9225094b5
Stanislav Korotaev <superstals@gmail.com>
2014-07-19 16:15:47 +0400

    Clickable task

---
+++ minild53/Assets/Scripts/TaskContainer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskContainer : MonoBehaviour {
+
+	UISlider slider;
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<UISlider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void increaseValue()
+	{
+		slider.value = slider.value + 0.1f;
+		if(slider.value >= 1){
+			slider.value -= 1;
+		}
+	}
+}
