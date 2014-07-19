c639409c7ea275e62f48b1b0b6fb8dcba495624d
Stanislav Korotaev <superstals@gmail.com>
2014-07-19 19:55:47 +0400

    lerp for energy

---
+++ minild53/Assets/Scripts/EnergyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyController : MonoBehaviour {
+	UISlider progressBar;
+
+	// Use this for initialization
+	void Start () {
+		progressBar = GetComponent<UISlider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		float targetValue = Game.Instance.getPlayer ().getCurrentEnergyFactor ();
+		progressBar.value = Mathf.Lerp(progressBar.value, targetValue, Time.deltaTime * 5f);
+	}
+}
--- minild53/Assets/Scripts/GuiUpdater.cs
+++ minild53/Assets/Scripts/GuiUpdater.cs
@@ -24,8 +24,6 @@ public class GuiUpdater : MonoBehaviour {
 		moneyLabel.text = moneyToString (Game.Instance.getPlayer ().money);
 
 		ageLabel.text = "Age: " + Game.Instance.getPlayer().age.asString ();
-
-		energyBar.value = Game.Instance.getPlayer().getCurrentEnergyFactor ();
 	}
 
 	string moneyToString(int money)
