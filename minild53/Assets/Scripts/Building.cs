bf33f58fc8e6b563279e0d547bf4aa5440b66e3e
Stanislav Korotaev <superstals@gmail.com>
2014-07-19 12:42:42 +0400

    empty scripts test

---
+++ minild53/Assets/Scripts/Building.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
---
+++ minild53/Assets/Scripts/Task.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Task {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+}
