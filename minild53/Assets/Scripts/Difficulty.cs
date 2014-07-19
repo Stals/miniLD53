0cb72101d67a3ffed2dd705faab7483f0e24ae21
Stanislav Korotaev <superstals@gmail.com>
2014-07-19 21:22:09 +0400

    difficulty function first version

--- minild53/Assets/Scripts/Difficulty.cs
+++ minild53/Assets/Scripts/Difficulty.cs
@@ -9,10 +9,23 @@ public class Difficulty {
 	public string name { get; set; }
 	public List<TaskEffect> effects;
 
+	private const float E_CONST = 2.7182f;
+
 	// 
 	public List<TaskEffect> getEffectsForLevel(int buildingLevel)
 	{
-		return effects;
+		if (buildingLevel > 1) {
+			List<TaskEffect> changedEffects = effects.GetRange (0, effects.Count);
+			foreach (TaskEffect effect in changedEffects) {
+				// TODO round?
+				effect.amount *= (int)(buildingLevel * E_CONST);
+			}
+
+			return changedEffects;
+
+		} else {
+			return effects;
+		}
 	}
 
 }
